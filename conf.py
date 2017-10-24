d2d6e846c74da892d079d1801976c88695f85ec2
AJP <kranzuft@gmail.com>
2017-10-24 18:37:29 +1100

    Attempt to solve pdf problem

--- conf.py
+++ conf.py
@@ -32,6 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = []
+latex_keep_old_macro_names=False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
